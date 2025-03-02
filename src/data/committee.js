a1484f81c94c0844e8bd94ee54e162abb04f5174
Avipsa-Biswal <cse.22bcse37@silicon.ac.in>
2025-03-02 14:22:41 +0530

    agenda updated

--- src/data/committee.js
+++ src/data/committee.js
@@ -3,7 +3,7 @@ export const committees = [
       name: "UNHRC",
       url: "https://ik.imagekit.io/Litfest2025/Group%20807.png?updatedAt=1738925003010",
       body: "The United Nations Human Rights Council (UNHRC) is the platform which guides global actions towards a future of guaranteed human rights. The council is one of the main UN organs within the United Nations and deals primarily with the protection and promotion of human rights. A subsidiary body of the General Assembly, it was established on 15 March 2006. The Council investigates allegations of breaches of human rights in United Nations member states and addresses thematic human rights issues like freedom of association and assembly, freedom of expression, freedom of belief and religion, women's rights, LGBT rights, and the rights of racial and ethnic minorities.",
-      agenda:"To be revealed",
+      agenda:"Reviewing Human Rights Violations wrt Women, LGBTQ+ and other Vulnerable Minorities and Envisaging Solutions.",
         // "Combating intolerance, negative stereotyping and stigmatization of, and discrimination, incitement to violence and violence against, persons based on religion or belief.",
       ebs: [
         {
@@ -24,7 +24,7 @@ export const committees = [
       name: "DISEC",
       url: "https://ik.imagekit.io/Litfest2025/Mask%20group%20(1).png?updatedAt=1738925002787",
       body: "The first committee of the United Nations General Assembly deals with disarmament, global challenges and threats to peace that affect the international community and seeks out solutions to the challenges in the international security regime. It considers all disarmament and international security matters within the scope of the Charter or relating to the powers and functions of any other organ of the United Nations; the general principles of cooperation in the maintenance of international peace and security, as well as principles governing disarmament and the regulation of armaments; promotion of cooperative arrangements and measures aimed at strengthening stability through lower levels of armaments.",
-      agenda:"To be revealed",
+      agenda:"Strengthening the role of mediation in the peaceful settlement of disputes, conflict prevention and resolution.",
         // "Deliberating upon the question of Lethal Autonomous Weapon Systems (LAWS) and Cyber warfare, in light of the Law of Armed Conflict.",
       ebs: [
         {
@@ -45,7 +45,7 @@ export const committees = [
       name: "WHA",
       url: "https://ik.imagekit.io/Litfest2025/Mask%20group%20(1).png?updatedAt=1738925002787",
       body: "The World Health Assembly (WHA) is the World Health Organization's (WHO) main legislative body. Established in 1948, it comprises up to three delegates from each UN member state. The WHA determines policies, appoints the Director-General, and supervises financial policies. It discusses legislation on health issues and declares global health emergencies, such as the Ebola and COVID-19 pandemics. With 194 member states, the WHA promotes international cooperation to address public health challenges.",
-      agenda:"To be revealed",
+      agenda:"Reviewing the effectiveness of the Mental Health Gap Action Programme and addressing the inconsistencies surrounding the initiative",
         // "Progressing towards a sustainable future by addressing gender disparities through education and economic development.",
       ebs: [{
         name: "Nishant Dash",
